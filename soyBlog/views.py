ae751ff092a36d9632dd08cbce99e87aad58e3e3
soy <zhuo-wy@163.com>
2017-11-10 11:58:34 +0800

    使用了更快的mistune；使用highlight.js提供了代码高亮。

--- soyBlog/views.py
+++ soyBlog/views.py
@@ -2,12 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 from math import ceil
-from markdown import markdown
+from mistune import markdown
 from .models import Post
 
-def md2html(md_string):
-
+def safe_markdown(md_string):
+  return mark_safe(markdown(md_string))
 
 def home(request, page=1):
   context = {}
@@ -21,9 +22,9 @@ def home(request, page=1):
   max_page = ceil(page_count/post_perpage)
 
   page = page if page > 0 else 1
-  context['posts'] = Post.objects.all()[(page - 1) * post_perpage : post_perpage]
+  context['posts'] = Post.objects.order_by('-id')[(page - 1) * post_perpage : post_perpage]
   for post in context['posts']:
-    post.content = markdown(post.content)
+    post.content = safe_markdown(post.content)
   context['next_page'] = page + 1 if page < max_page else None
   context['prev_page'] = page - 1 if page > 1 else None
 
